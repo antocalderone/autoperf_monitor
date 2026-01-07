41bb15d9ea30d071a3a6faf84794d2bbc2dc1896
Antonio Calderone <antocalderone@gmail.com>
2026-01-07 12:05:25 +0100

    feat: Initial project setup for AutoPerf Monitor

---
+++ README.md
@@ -0,0 +1,3 @@
+# AutoPerf Monitor
+
+An Android Flutter application for real-time car performance monitoring and fuel tracking.
---
+++ android/app/src/main/kotlin/com/example/autoperf_monitor/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.autoperf_monitor
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
